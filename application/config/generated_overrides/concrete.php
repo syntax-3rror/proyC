8b8639ace8cd0ed8fea7d12a3992c4d43063adda
syntax-3rror <naty.dany.98@gmail.com>
2015-05-30 16:26:28 -0400

    primer comit

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-05-29T06:12:23+02:00
+ *
+ * @item      misc.do_page_reindex_check
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'site' => 'proyecto',
+    'version_installed' => '5.7.4.2',
+    'misc' => array(
+        'access_entity_updated' => 1432861857,
+        'do_page_reindex_check' => false
+    )
+);
---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/2e5dd60b60fde061/580512b7c68b715d.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_0dc639c23550fd4eb93389aa1924275a_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1433286878;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1432864372;
